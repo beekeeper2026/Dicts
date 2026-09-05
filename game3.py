27cdbfda77050b3f6e483c0347be2a9f9c3f9665
beekeeper2026 <michael.hinck@yahoo.de>
2026-09-05 12:22:26 +0200

    Add files via upload

--- game3.py
+++ game3.py
@@ -1,18 +1,22 @@
 import pgzrun
 
 WIDTH = 800
-HEIGTH = 800
+HEIGHT = 800
 x = 0
 y = 0
 
 def draw():
     global x, y
+    #  row 1
     for i in range(8):
-        r = Rect((x, y), (50, 50))
-        screen.draw.filled_rect(r, "White")
-        x += 100
-
-
+        x=0
+        for j in range(8):
+            if (i + j) % 2 == 0:
+                r = Rect((x, y), (100, 100))
+                screen.draw.filled_rect(r, "White")
+            x += 100
+        y+=100
+        
 
 def update():
     pass
